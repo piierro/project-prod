a7c4b1e721497d4ffc1d95113ebde27dddfa2569
Kira Murzaneva <marccappy76@gmail.com>
2024-08-13 22:10:59 +0300

    methodology feature-sliced

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import { Link, Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { Suspense } from 'react';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from './providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 
 const App = () => {
@@ -17,8 +18,8 @@ const App = () => {
         <Link to={'/about'}>О сайте</Link>
         <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-                <Route path={'/about'} element={<AboutPageAsync />}></Route>
-                <Route path={'/'} element={<MainPageAsync />}></Route>
+                <Route path={'/about'} element={<AboutPage />}></Route>
+                <Route path={'/'} element={<MainPage />}></Route>
         </Routes>
         </Suspense>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './app/App';
 import { BrowserRouter } from 'react-router-dom';
-import ThemeProvider from './theme/ThemeProvider';
+import { ThemeProvider } from 'app/providers/ThemeProvider';
+
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
