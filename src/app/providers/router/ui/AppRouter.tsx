db77eb0b4f5ca1739a415d83535a2834e71366ad
Kira Murzaneva <marccappy76@gmail.com>
2024-08-15 13:34:55 +0300

    Sidebar

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -4,17 +4,21 @@ import { routeConfig } from 'shared/config/routeConfig/routeConfig'
 
 const AppRouter = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
+      <Routes>
             {Object.values(routeConfig).map(({element, path}) => (
               <Route 
                  key={path}
                  path={path}
-                 element={element}
+                 element={(
+                  <Suspense fallback={<div>Loading...</div>}>
+                    <div className='page-wrapper'>
+                      {element}
+                    </div>
+                  </Suspense>
+                 )}
               />
             ))}
-        </Routes>
-    </Suspense>
+      </Routes>
   )
 }
 
---
+++ src/widgetes/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,25 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import * as cls from './Sidebar.module.scss';
+import { useState } from 'react';
+import { ThemeSwitcher } from 'widgetes/ThemeSwitcher';
+
+interface SidebarProps {
+    className?: string 
+}
+
+export const Sidebar = ({className}: SidebarProps) => {
+    const [collapsed, setCollapsed] = useState(false);
+
+    const onToggle = () => {
+        setCollapsed(prev => !prev)
+    }
+  
+  return (
+    <div className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}>
+        <button onClick={onToggle}>Click</button>
+        <div className={cls.switchers}>
+          <ThemeSwitcher />
+        </div>
+    </div>
+  )
+}
