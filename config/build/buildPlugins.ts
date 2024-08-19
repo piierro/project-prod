46939c204a7021fcce74e2371bd22ce0e6727d8e
Kira Murzaneva <marccappy76@gmail.com>
2024-08-19 18:23:33 +0300

    add loader and notfound

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -16,14 +16,8 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
+    new webpack.HotModuleReplacementPlugin()
   ];
-
-  if (isDev) {
-    plugins.push(new webpack.HotModuleReplacementPlugin());
-    // plugins.push(new BundleAnalyzerPlugin({
-    //   openAnalyzer: false,
-    // }));
-  }
 
   return plugins;
 }
--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { routeConfig } from 'shared/config/routeConfig/routeConfig'
+import { PageLoader } from 'widgetes/PageLoader/ui/PageLoader'
 
 const AppRouter = () => {
   return (
@@ -10,7 +11,7 @@ const AppRouter = () => {
           key={path}
           path={path}
           element={(
-            <Suspense fallback={<div>Loading...</div>}>
+            <Suspense fallback={<PageLoader />}>
               <div className='page-wrapper'>
                 {element}
               </div>
