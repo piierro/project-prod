3f731015ed5074ec64a25d826cadffa4aa07b102
Kira Murzaneva <marccappy76@gmail.com>
2024-08-20 19:41:54 +0300

    fix devs plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -16,12 +16,13 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false
     })
   ];
+
+  if(isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new BundleAnalyzerPlugin({openAnalyzer: false}))
+  }
 
   return plugins;
 }
