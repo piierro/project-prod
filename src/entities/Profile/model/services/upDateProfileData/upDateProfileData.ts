42ab9d02d437cafcdc1de19465bed52fe0ca5201
Kira Murzaneva <marccappy76@gmail.com>
2024-09-12 19:48:11 +0300

    add validate profile errors

--- src/entities/Profile/model/services/upDateProfileData/upDateProfileData.ts
+++ src/entities/Profile/model/services/upDateProfileData/upDateProfileData.ts
@@ -1,14 +1,20 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ThunkConfig } from 'app/providers/StoreProvider';
-import { Profile } from '../../types/profile';
+import { Profile, ValidateProfileError } from '../../types/profile';
 import { getProfileForm } from '../../selectors/getProfileForm/getProfileForm';
+import { validateProfileData } from '../validateProfileData/validateProgileData';
 
-export const upDateProfileData = createAsyncThunk<Profile, void, ThunkConfig<string>>(
+export const upDateProfileData = createAsyncThunk<Profile, void, ThunkConfig<ValidateProfileError[]>>(
   'profile/upDateProfileData',
   async (_, thunkApi) => {
     const { extra, rejectWithValue, getState } = thunkApi;
 
-    const formData = getProfileForm(getState())
+    const formData = getProfileForm(getState());
+    const errors = validateProfileData(formData);
+
+    if(errors.length) {
+      return rejectWithValue(errors);
+    }
 
     try {
       const response = await extra.api.put<Profile>('/profile', formData);
@@ -16,7 +22,7 @@ export const upDateProfileData = createAsyncThunk<Profile, void, ThunkConfig<str
       return response.data;
     } catch (e) {
       console.log(e);
-      return rejectWithValue('error');
+      return rejectWithValue([ValidateProfileError.SERVER_ERROR]);
     }
   },
 );
