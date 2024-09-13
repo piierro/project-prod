5bec7e0b57e50c5b818d2c31d3e9ffdb6fb82add
Kira Murzaneva <marccappy76@gmail.com>
2024-09-13 14:58:13 +0300

    add concurrently

--- json-server/index.js
+++ json-server/index.js
@@ -6,9 +6,9 @@ import fs from 'fs';
 import jsonServer from 'json-server';
 import path from 'path';
 
-const server = jsonServer.create();
-
 const __dirname = path.resolve();
+
+const server = jsonServer.create();
 
 const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
 
